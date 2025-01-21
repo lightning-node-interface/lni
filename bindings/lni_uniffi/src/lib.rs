6a13c984f1454e7a38db873e5ac5fce9cded17ba
nicktee <ntheile@gmail.com>
2025-01-21 01:42:42 -0600

    .

--- bindings/lni_uniffi/src/lib.rs
+++ bindings/lni_uniffi/src/lib.rs
@@ -9,5 +9,13 @@ use crate::error::Result;
 pub use api_client::Fetcher;
 pub use phoenixd::PhoenixdNode;
 
+pub struct Feenix {}
+
+impl Feenix {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
 // for use with uniffi decorators (not udl files)
 uniffi::setup_scaffolding!("lni_sdk");
