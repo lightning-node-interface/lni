73ac6576f85623e34acc3cb2a595a34409888ea8
nicktee <ntheile@gmail.com>
2025-04-04 21:39:39 -0500

    .

--- crates/lni/lnd/lib.rs
+++ crates/lni/lnd/lib.rs
@@ -13,7 +13,6 @@ use crate::{
 pub struct LndConfig {
     pub url: String,
     pub macaroon: String,
-    #[uniffi(default = Some(None))]
     pub socks5_proxy: Option<String>, // socks5h://127.0.0.1:9150
     #[uniffi(default = Some(true))]
     pub accept_invalid_certs: Option<bool>,
